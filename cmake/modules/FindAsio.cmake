80cf04e6ead4c8f3a4d41d645c2613e9aa0cd689
Michael Franzen <franzenmic@gmail.com>
2017-02-15 21:45:15 +0100

    Changed the CMake-directives a little bit for compilation on Windows, updated README.md

--- cmake/modules/FindAsio.cmake
+++ cmake/modules/FindAsio.cmake
@@ -1,25 +1,26 @@
 if (NOT ASIO_FOUND)
-	# Try standalone Asio first
-	find_path (ASIO_INCLUDE_DIR NAMES asio HINTS "/usr/include" "/usr/local/include" "/opt/local/include")
+  # Try standalone Asio first
+  find_path (ASIO_INCLUDE_DIR NAMES asio HINTS "/usr/include" "/usr/local/include" "/opt/local/include")
 
-	if (NOT ASIO_INCLUDE_DIR)
-	  # Try Boost.Asio
-	  find_package(Boost COMPONENTS system REQUIRED)
-	  set (ASIO_STANDALONE FALSE)
-	else()
-	  set (ASIO_STANDALONE TRUE)
-	endif()
+  if (NOT ASIO_INCLUDE_DIR)
+    # Try Boost.Asio
+    find_package(Boost COMPONENTS system REQUIRED)
+    set (ASIO_STANDALONE FALSE)
+  else()
+    set (ASIO_STANDALONE TRUE)
+  endif()
 
-	if (ASIO_STANDALONE)
-		add_library(asio INTERFACE IMPORTED)
-		set_target_properties(asio PROPERTIES
-			INTERFACE_INCLUDE_DIRECTORIES ${ASIO_INCLUDE_DIR}
-		)
-	else()
-		add_library(asio INTERFACE IMPORTED)
-		set_target_properties(asio PROPERTIES
-			INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIR}
-			INTERFACE_LINK_LIBRARIES ${Boost_SYSTEM_LIBRARY}
-		)
-	endif()
+  if (ASIO_STANDALONE)
+    add_library(asio INTERFACE IMPORTED)
+    set_target_properties(asio PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES ${ASIO_INCLUDE_DIR}
+    )
+  else()
+    add_library(asio INTERFACE IMPORTED)
+    set_target_properties(asio PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIR}"
+      INTERFACE_LINK_LIBRARY "${Boost_SYSTEM_LIBRARY}"
+    )
+    message(WARNING "${Boost_SYSTEM_LIBRARY}")
+  endif()
 endif()
